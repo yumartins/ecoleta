0ec2f15c94ef551d65ebe2db82ce12f3e0917ca0
yumartins <yuri@estudioflow.com.br>
2020-06-06 20:20:18 -0300

    feat: connection database sqlite

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import router from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+app.use(router);
 
 app.listen(5000);
