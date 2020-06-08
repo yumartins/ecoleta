6f6e09983b79398ceb8902c743ad9f3d115aa8eb
yumartins <yuri@estudioflow.com.br>
2020-06-07 21:22:07 -0300

    feat: list items

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,22 @@
 import express from 'express';
+import knex from './database/connection';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.json({ message: 'Hello World' }));
+
+router.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serialized = items.map((item, index) => {
+    return {
+      id: index,
+      title: item.title,
+      image: `http://localhost:5000/uploads/${item.image}`,
+    };
+  });
+
+  return res.json(serialized);
+});
 
 export default router;
