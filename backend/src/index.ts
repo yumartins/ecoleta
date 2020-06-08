123262215038d0362a2ebad978c31d8705b51c20
yumartins <yuri@estudioflow.com.br>
2020-06-07 21:07:56 -0300

    feat: create uploads routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import router from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use('/uploads', express.static(
+  path.resolve(__dirname, '..', 'uploads'),
+));
 
 app.listen(5000);
